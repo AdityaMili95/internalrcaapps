2198ce630a5c54687cb4f2cd13729162ef8dac80
Aditya Mili <adityamili@Adityas-MacBook-Pro.local>
2021-02-22 16:16:19 +0700

    improve lots of stuff

--- app.go
+++ app.go
@@ -143,11 +143,7 @@ func RegisterCron() {
 			Job{
 				Interval: interval,
 				Handler: func() {
-					msg, err := GetRCADataWrapper(channelID, 0)
-					if err != nil {
-						Println(nil, "PROCESS CRON ERROR, err: ", err)
-						return
-					}
+					msg := ConstructRCADataString(v, 0, "")
 					NotifySlack(msg, v.ChannelKey)
 				},
 			})
@@ -223,10 +219,6 @@ func (api API) Register(router *httprouter.Router) {
 		api.HandleCommand,
 	)
 
-	router.GET("/rca",
-		api.HandleCommand,
-	)
-
 }
 
 func (api API) HandleCommand(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
@@ -234,7 +226,7 @@ func (api API) HandleCommand(w http.ResponseWriter, r *http.Request, p httproute
 	command := r.FormValue("command")
 	text := r.FormValue("text")
 	uname := r.FormValue("user_name")
-	responseUrl := r.FormValue("response_url")
+	//responseUrl := r.FormValue("response_url")
 
 	if command == "" || channelID == "" || uname == "" {
 		resp := Response{
@@ -248,39 +240,49 @@ func (api API) HandleCommand(w http.ResponseWriter, r *http.Request, p httproute
 	}
 
 	var err error
-	msg := ""
+	directMsg := ""
+	ephemeralMsg := ""
+	directChannelKey := ""
 
 	tempSlackMsg := SlackMsgStructure{}
-	shouldSlackNotify := false
+	channelData := Channel{}
 
-	if command == "/listrca" {
-		tempSlackMsg, err = GetRCADataWrapper(channelID, 0)
-		msg = fmt.Sprintf("RCA List requested by %s", uname)
-		shouldSlackNotify = true
-	} else if command == "/listdonerca" {
-		tempSlackMsg, err = GetRCADataWrapper(channelID, 1)
-		msg = fmt.Sprintf("RCA List requested by %s", uname)
-		shouldSlackNotify = true
-	} else if command == "/addrca" {
-		msg, err = AddRCA(uname, text, channelID)
-	} else if command == "/donerca" {
-		msg, err = DoneRCA(uname, text, channelID, 1)
-	} else if command == "/removerca" {
-		msg, err = DoneRCA(uname, text, channelID, 3)
-	} else if command == "/doneallrca" {
-		msg, err = DoneAllRCA(uname, channelID)
-	} else if command == "/setscheduler" {
-		msg, err = SetScheduler(uname, channelID, text)
-	} else if command == "/removescheduler" {
-		msg, err = SetScheduler(uname, channelID, "")
-	} else if command == "/setslackwebhook" {
-		msg, err = SetWebhook(uname, channelID, text)
-	} else if command == "/setfooter" {
-		msg, err = SetFooter(uname, channelID, text)
-	} else if command == "/setpma" {
-		msg, err = SetPMA(uname, channelID, text)
-	} else if command == "/internalrcahelp" {
-		msg = HelpRCA()
+	if WebhookRequiredCommand(command) {
+		channelData, err = GetRCAData(channelID)
+
+		if err == nil && channelData.ChannelKey == "" {
+			err = errors.New("Channel Webhook not set, set using command /setslackwebhook [webhook_key]")
+		} else if err == nil {
+			directChannelKey = channelData.ChannelKey
+		}
+	}
+
+	if err == nil {
+		if command == "/listrca" {
+			tempSlackMsg = ConstructRCADataString(channelData, 0, uname)
+		} else if command == "/listdonerca" {
+			tempSlackMsg = ConstructRCADataString(channelData, 1, uname)
+		} else if command == "/addrca" {
+			directMsg, err = AddRCA(uname, text, channelID)
+		} else if command == "/donerca" {
+			directMsg, err = DoneRCA(uname, text, channelID, 1)
+		} else if command == "/removerca" {
+			directMsg, err = DoneRCA(uname, text, channelID, 3)
+		} else if command == "/doneallrca" {
+			directMsg, err = DoneAllRCA(uname, channelID, channelData)
+		} else if command == "/setscheduler" {
+			directMsg, err = SetScheduler(uname, channelID, text)
+		} else if command == "/removescheduler" {
+			directMsg, err = SetScheduler(uname, channelID, "")
+		} else if command == "/setslackwebhook" {
+			directMsg, err = SetWebhook(uname, channelID, text)
+		} else if command == "/setfooter" {
+			directMsg, err = SetFooter(uname, channelID, text)
+		} else if command == "/setpma" {
+			directMsg, err = SetPMA(uname, channelID, text)
+		} else if command == "/internalrcahelp" {
+			ephemeralMsg = HelpRCA()
+		}
 	}
 
 	if err != nil {
@@ -288,21 +290,32 @@ func (api API) HandleCommand(w http.ResponseWriter, r *http.Request, p httproute
 			ResponseType: "ephemeral",
 			Text:         err.Error(),
 		}
-
 		WriteResponse(w, resp)
 		return
 	}
 
-	if shouldSlackNotify {
-		NotifySlack(tempSlackMsg, responseUrl)
+	if len(tempSlackMsg.Blocks) > 0 {
+		NotifySlack(tempSlackMsg, directChannelKey)
+		return
 	}
 
-	tempSlackResp := SlackMsgStructure{}
-	tempblock := GetSlackMessageStructure(msg)
-	tempSlackResp.Blocks = append(tempSlackResp.Blocks, tempblock)
+	if directMsg != "" {
+		tempblock := GetSlackMessageStructure(directMsg)
+		tempSlackMsg.Blocks = append(tempSlackMsg.Blocks, tempblock)
+		NotifySlack(tempSlackMsg, directChannelKey)
+		return
+	}
 
-	WriteResponse(w, tempSlackResp)
+	if ephemeralMsg != "" {
+		tempblock := GetSlackMessageStructure(ephemeralMsg)
+		tempSlackMsg.Blocks = append(tempSlackMsg.Blocks, tempblock)
+		WriteResponse(w, tempSlackMsg)
+	}
 
+}
+
+func WebhookRequiredCommand(command string) bool {
+	return command != "/internalrcahelp" && command != "/setslackwebhook"
 }
 
 func WriteResponse(w http.ResponseWriter, response interface{}) {
@@ -321,20 +334,11 @@ func NotifySlack(message SlackMsgStructure, channelKey string) error {
 }
 
 func HelpRCA() string {
-	return "*Internal RCA BOT Command Help*\n\n• `/listrca` - Get List Active RCA :memo::memo:\n• `/listdonerca` - Get list of Done RCA\n• `/addrca - Title Desc Assignee [PMATicketURL] [Staging|Production]` - Add New RCA\n• `/removerca issueID` - Remove RCA\n• `/donerca issueID` - Set RCA to Done\n• `/doneallrca` - *Done all* active RCA :warning::warning:\n• `/setpma issueID PMATicketURL` - Set PMA Ticket for issue\n• `/setscheduler schedule` (*<https://pkg.go.dev/github.com/robfig/cron/v3|format>*) - Set Scheduler for RCA List\n• `/setslackwebhook webhookUrl` - Set slack webhook for scheduler (*for the webhook url*, contact: <@U75J4HEF9>)\n• `/removescheduler` - Remove Scheduler for RCA List \n• `/setfooter text` - Set *Custom* footer notes that shown at the bottom of RCA List\n• `/internalrcahelp` - Command list for RCA & Sharing Bot"
+	return "*Internal RCA BOT Command Help*\n\n• `/listrca` - Get List Active RCA :memo::memo:\n• `/listdonerca` - Get list of Done RCA\n• `/addrca - Title Desc Assignee [PMATicketURL] [Staging|Production]` - Add New RCA\n• `/removerca issueID` - Remove RCA\n• `/donerca issueID` - Set RCA to Done\n• `/doneallrca` - *Done all* active RCA :warning::warning:\n• `/setpma issueID PMATicketURL` - Set PMA Ticket for issue\n• `/setscheduler schedule` (*<https://pkg.go.dev/github.com/robfig/cron/v3|format>*) - Set Scheduler for RCA List\n• `/setslackwebhook webhook_key` - Set slack webhook for scheduler (*for the webhook url*, contact: <@U75J4HEF9>)\n• `/removescheduler` - Remove Scheduler for RCA List \n• `/setfooter text` - Set *Custom* footer notes that shown at the bottom of RCA List\n• `/internalrcahelp` - Command list for RCA & Sharing Bot"
 }
 
 func SetScheduler(uname, channelID, text string) (string, error) {
 	ctx := context.Background()
-
-	v, err := GetRCAData(channelID)
-	if err != nil {
-		return "", err
-	}
-
-	if v.ChannelKey == "" {
-		return "", errors.New("Channel Webhook not set, set using command /setslackwebhook [webhook_key]")
-	}
 
 	updateTxn := func(node db.TransactionNode) (interface{}, error) {
 		return text, nil
@@ -346,8 +350,8 @@ func SetScheduler(uname, channelID, text string) (string, error) {
 		action = fmt.Sprintf("set to %s", text)
 	}
 
-	msg := fmt.Sprintf("RCA List scheduler %s by %s", action, uname)
-	err = FirebaseClient.NewRef(fmt.Sprintf("Scheduler/%s", channelID)).Transaction(ctx, updateTxn)
+	msg := fmt.Sprintf("_RCA List scheduler %s by %s_", action, uname)
+	err := FirebaseClient.NewRef(fmt.Sprintf("Scheduler/%s", channelID)).Transaction(ctx, updateTxn)
 
 	if err == nil {
 		RegisterCron()
@@ -364,7 +368,7 @@ func SetFooter(uname, channelID, text string) (string, error) {
 		return text, nil
 	}
 
-	return fmt.Sprintf("Message RCA - updated to %s by %s", strings.Replace(text, "\n", "", -1), uname), FirebaseClient.NewRef(fmt.Sprintf("Channel/%s/Footer", channelID)).Transaction(ctx, updateTxn)
+	return fmt.Sprintf("_Message RCA - updated to %s by %s_", strings.Replace(text, "\n", "", -1), uname), FirebaseClient.NewRef(fmt.Sprintf("Channel/%s/Footer", channelID)).Transaction(ctx, updateTxn)
 }
 
 func SetWebhook(uname, channelID, text string) (string, error) {
@@ -374,24 +378,19 @@ func SetWebhook(uname, channelID, text string) (string, error) {
 		return text, nil
 	}
 
-	return fmt.Sprintf("Slack Webook set by %s", uname), FirebaseClient.NewRef(fmt.Sprintf("Channel/%s/channelKey", channelID)).Transaction(ctx, updateTxn)
+	return fmt.Sprintf("_Slack Webook set by %s_", uname), FirebaseClient.NewRef(fmt.Sprintf("Channel/%s/channelKey", channelID)).Transaction(ctx, updateTxn)
 }
 
-func DoneAllRCA(uname, channelID string) (string, error) {
+func DoneAllRCA(uname, channelID string, channelData Channel) (string, error) {
 
-	v, err := GetRCAData(channelID)
-	if err != nil {
-		return "", err
-	}
-
-	for issueID, _ := range v.Data {
-		err = SetDoneRCAData(channelID, issueID, 1)
+	for issueID, _ := range channelData.Data {
+		err := SetDoneRCAData(channelID, issueID, 1)
 		if err != nil {
 			return "", err
 		}
 	}
 
-	return fmt.Sprintf("All RCA Set to Done by %s", uname), nil
+	return fmt.Sprintf("_All RCA Set to Done by %s_", uname), nil
 }
 
 func DoneRCA(uname, text, channelID string, status int) (string, error) {
@@ -418,7 +417,7 @@ func DoneRCA(uname, text, channelID string, status int) (string, error) {
 		return "", errors.New(fmt.Sprintf("FAILED - Invalid RCA ID (%s) - Action: %s by %s", desc[0], setTo, uname))
 	}
 
-	return fmt.Sprintf("RCA %s (`%s`) %s by %s", v.Title, desc[0], setTo, uname), SetDoneRCAData(channelID, desc[0], status)
+	return fmt.Sprintf("_RCA %s (`%s`) %s by %s_", v.Title, desc[0], setTo, uname), SetDoneRCAData(channelID, desc[0], status)
 }
 
 func SetPMA(uname, text, channelID string) (string, error) {
@@ -444,7 +443,7 @@ func SetPMA(uname, text, channelID string) (string, error) {
 		return desc[1], nil
 	}
 
-	return fmt.Sprintf("RCA %s (`%s`) PMA Ticket set by %s", v.Title, desc[0], uname), FirebaseClient.NewRef(fmt.Sprintf("Channel/%s/data/%s/PMA", channelID, desc[0])).Transaction(ctx, updateTxn)
+	return fmt.Sprintf("_RCA %s (`%s`) PMA Ticket set by %s_", v.Title, desc[0], uname), FirebaseClient.NewRef(fmt.Sprintf("Channel/%s/data/%s/PMA", channelID, desc[0])).Transaction(ctx, updateTxn)
 }
 
 func SetDoneRCAData(channelID, issueID string, status int) error {
@@ -489,7 +488,7 @@ func AddRCA(uname, text, channelID string) (string, error) {
 		return "", err
 	}
 
-	return fmt.Sprintf("RCA %s Added by %s", desc[0], uname), nil
+	return fmt.Sprintf("_RCA %s Added by %s_", desc[0], uname), nil
 }
 
 func CaptureCronPanic(handler func()) func() {
@@ -556,16 +555,6 @@ func (c *Cron) Run(taskCron *cron.Cron) {
 	taskCron.Start()
 }
 
-func GetRCADataWrapper(channelID string, getStatus int) (SlackMsgStructure, error) {
-	v, err := GetRCAData(channelID)
-
-	if err != nil {
-		return SlackMsgStructure{}, err
-	}
-
-	return ConstructRCADataString(v, getStatus), nil
-}
-
 func GetRCAData(channelID string) (Channel, error) {
 	var v Channel
 	ctx := context.Background()
@@ -573,8 +562,15 @@ func GetRCAData(channelID string) (Channel, error) {
 	return v, err
 }
 
-func ConstructRCADataString(v Channel, getStatus int) SlackMsgStructure {
-	title := fmt.Sprintf("*Internal Sharing & RCA List*\n\n")
+func ConstructRCADataString(v Channel, getStatus int, uname string) SlackMsgStructure {
+
+	title := ""
+
+	if uname != "" {
+		title += fmt.Sprintf("_RCA List requested by %s_\n\n", uname)
+	}
+
+	title += fmt.Sprintf("*Internal Sharing & RCA List*\n\n")
 
 	staging := ""
 	production := ""
@@ -582,7 +578,7 @@ func ConstructRCADataString(v Channel, getStatus int) SlackMsgStructure {
 	emot := "bangbang"
 
 	if getStatus == 1 {
-		title = fmt.Sprintf("*Internal Sharing & RCA List - DONE*\n\n")
+		title += fmt.Sprintf("*Internal Sharing & RCA List - DONE*\n\n")
 		emot = "white_check_mark"
 	}
 
@@ -714,7 +710,7 @@ func startProcessCron() {
 	}
 
 	for _, v := range channels {
-		slackMsg := ConstructRCADataString(v, 0)
+		slackMsg := ConstructRCADataString(v, 0, "")
 		NotifySlack(slackMsg, v.ChannelKey)
 	}
 }
